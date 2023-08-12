d6467255b4189322f6eb31aa7a4a58aa7fdc754b
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-08-12 21:22:37 +0300

    Done task 2 (unknown console errors)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import common from './common.json';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.ready().then(onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onPlay() {
+  const time = JSON.parse(localStorage.getItem(common.LS_CURRENTTIME) ?? "0");
+  player.setCurrentTime(time);
+}
+ 
+function onTimeUpdate() {
+  player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem(common.LS_CURRENTTIME, JSON.stringify(seconds));
+  })
+}
